65c9e6716e850ae02ad1ede6e4585cde30b37b74
Nate Mackay <mac17039@byui.edu>
2020-09-04 18:14:19 -0600

    add 2021 come follow me schedule

--- db/cfmdb.sql
+++ db/cfmdb.sql
@@ -352,7 +352,7 @@ INSERT INTO Topic (title) VALUES ('The Family Is Central to the Creator''s Plan'
 INSERT INTO Topic (title) VALUES ('The Matchless Gift of God''s Divine Son');
 
 
-INSERT INTO background VALUES ('Joseph.jpg');
-INSERT INTO background VALUES ('https://upload.wikimedia.org/wikipedia/commons/c/c8/Joseph_Smith_first_vision_stained_glass.jpg');
-INSERT INTO background VALUES ('https://www.sltrib.com/resizer/hxyw7axVp11boJEAibpdlp10Y-0=/0x600/smart/filters:quality(86)/arc-anglerfish-arc2-prod-sltrib.s3.amazonaws.com/public/PJQ6RUV7HJBUPKPT63HXIHJLI4.jpeg');
+INSERT INTO background (picture) VALUES ('Joseph.jpg');
+INSERT INTO background (picture) VALUES ('https://upload.wikimedia.org/wikipedia/commons/c/c8/Joseph_Smith_first_vision_stained_glass.jpg');
+INSERT INTO background (picture) VALUES ('https://www.sltrib.com/resizer/hxyw7axVp11boJEAibpdlp10Y-0=/0x600/smart/filters:quality(86)/arc-anglerfish-arc2-prod-sltrib.s3.amazonaws.com/public/PJQ6RUV7HJBUPKPT63HXIHJLI4.jpeg');
 
