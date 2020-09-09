21f82fc44777aa5c4626f402295894d33cd258a3
Nate Mackay <mac17039@byui.edu>
2020-09-09 17:01:22 -0600

    edit queries for kids points

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ express()
       console.log("received request for kids points");
 
       const client = await pool.connect()
-      const kids   = await client.query('SELECT * FROM Member');
+      const kids   = await client.query('SELECT * FROM Member ORDER BY id');
       const point  = await client.query('SELECT * FROM Points');
 
       const params = { 'kids'  : (kids)   ? kids.rows   : null, 
